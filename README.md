1f400682e2aa05336c7b22888b29d1b4c316cd7e
mlwebpublising <61729431+mlwebpublising@users.noreply.github.com>
2020-05-27 00:15:36 +0900

    -

--- README.md
+++
@@ -1,2 +0,0 @@
-# learningfactory
-learningfactory
---
+++ js/common.js
@@ -0,0 +1,152 @@
+$(document).ready(function() {
+
+    /* Swiper - Main visual */
+	var learningMainVisual = new Swiper('.swiper-container', {
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+          },
+		pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+	});    
+    /* Swiper - Main Course */
+	var learningMainCourse = new Swiper('.main_course_container', {
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 33,
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+        },
+        speed: 500,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+	});
+    
+    /* Swiper - Main New Course */
+	var mainNewCourse = new Swiper('.new_course_container', {
+        loop: true,
+        slidesPerView: 4,
+        spaceBetween: 33,
+        //autoplay: {
+        //    delay: 1500,
+        //    disableOnInteraction: false,
+        //},
+        speed: 500,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }
+	});
+    
+    /* Main Course Mouseover Event */
+    $(".main_course .swiper-slide").mouseover(function(){
+        $(this).addClass("on");
+    });
+    $(".main_course .swiper-slide").mouseleave(function(){
+        $(this).removeClass("on");
+    });
+    
+    /* Popular Course Tab */
+    popularTab();
+    recommendTab();
+    weeklyTab();
+    monthlyTab();
+});
+
+// Popular Course Tab
+function popularTab() {
+    var $tbtn = $(".tab_btn li");
+    var $tbtn_leng = $tbtn.length;
+    //$tbtn.width(100/$tbtn_leng+"%");
+    var $tab_contentsOb = $(".tab_contents");
+
+    $tab_contentsOb.hide();
+    $tab_contentsOb.eq(0).show();
+    $tbtn.eq(0).addClass("on");
+
+    $tbtn.click(function(e){
+    
+        e.preventDefault();
+
+        idx = $(this).index();
+
+        $tbtn.removeClass();
+        $tbtn.eq(idx).addClass("on");
+        $tab_contentsOb.hide();
+        $tab_contentsOb.eq(idx).show();       
+    });
+}
+// Popular Course Tab
+function recommendTab() {
+    var $tbtn = $(".tab_in_tab1 li");
+    var $tbtn_leng = $tbtn.length;
+    //$tbtn.width(100/$tbtn_leng+"%");
+    var $tab_contentsOb = $(".tab_course_desc1");
+
+    $tab_contentsOb.hide();
+    $tab_contentsOb.eq(0).show();
+    $tbtn.eq(0).addClass("on");
+
+    $tbtn.click(function(e){
+    
+        e.preventDefault();
+
+        idx = $(this).index();
+
+        $tbtn.removeClass();
+        $tbtn.eq(idx).addClass("on");
+        $tab_contentsOb.hide();
+        $tab_contentsOb.eq(idx).show();       
+    });
+}
+// Popular Course Tab
+function weeklyTab() {
+    var $tbtn = $(".tab_in_tab2 li");
+    var $tbtn_leng = $tbtn.length;
+    //$tbtn.width(100/$tbtn_leng+"%");
+    var $tab_contentsOb = $(".tab_course_desc2");
+
+    $tab_contentsOb.hide();
+    $tab_contentsOb.eq(0).show();
+    $tbtn.eq(0).addClass("on");
+
+    $tbtn.click(function(e){
+    
+        e.preventDefault();
+
+        idx = $(this).index();
+
+        $tbtn.removeClass();
+        $tbtn.eq(idx).addClass("on");
+        $tab_contentsOb.hide();
+        $tab_contentsOb.eq(idx).show();       
+    });
+}
+// Popular Course Tab
+function monthlyTab() {
+    var $tbtn = $(".tab_in_tab3 li");
+    var $tbtn_leng = $tbtn.length;
+    //$tbtn.width(100/$tbtn_leng+"%");
+    var $tab_contentsOb = $(".tab_course_desc3");
+
+    $tab_contentsOb.hide();
+    $tab_contentsOb.eq(0).show();
+    $tbtn.eq(0).addClass("on");
+
+    $tbtn.click(function(e){
+    
+        e.preventDefault();
+
+        idx = $(this).index();
+
+        $tbtn.removeClass();
+        $tbtn.eq(idx).addClass("on");
+        $tab_contentsOb.hide();
+        $tab_contentsOb.eq(idx).show();       
+    });
+}
