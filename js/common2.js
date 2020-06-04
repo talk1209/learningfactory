d52e5e30ebf29bd8b7b7b85712913dea4fc9a24b
leejun9717 <leejun@designest.co.kr>
2020-06-04 21:01:18 +0900

    고객센터 저장

--- js/common2.js
+++ js/common2.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-   	
+   	$('.table_faq_list').find('dt a').on('click', function(){
+		$td = $(this).parent().parent();
+		if($td.hasClass('on')){
+			$td.removeClass('on');
+		}else{
+			$td.addClass('on');
+		}
+	});
 });
