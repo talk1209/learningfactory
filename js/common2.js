58d48ea6021d8bd605001202779e649c34d45191
leejun9717 <leejun@designest.co.kr>
2020-06-13 18:06:57 +0900

    -

--- js/common2.js
+++ js/common2.js
@@ -51,26 +51,6 @@ $(document).ready(function() {
 		copyToClipboard($str);
 		alert('복사되었습니다.');
 	});
-
-     $('.autoheight').iframeHeight({
-
-       resizeMaxTry         : 2,
-       resizeWaitTime       : 300,
-       minimumHeight        : 100,
-       defaultHeight        : 100,
-       heightOffset         : 90,
-       exceptPages          : "",
-       debugMode            : false,
-       visibilitybeforeload : true,
-       blockCrossDomain     : true,
-       externalHeightName   : "bodyHeight",
-       onMessageFunctionName: "getHeight",
-       domainName           : "*",
-       watcher              : true,
-
-         watcherTime          : 400
-
-     });
 });
 
 // aside Tab
